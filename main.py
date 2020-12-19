7617bb643feb00f0afcdf9e6cea057bd1a00fb08
daivuong <paulminhdai@gmail.com>
2020-12-19 09:01:27 -0800

    Add check valid zipcode, print message when invalid zip

--- main.py
+++ main.py
@@ -9,41 +9,47 @@ load_dotenv(verbose=True)
 API_KEY = os.getenv('API_KEY')
 
 
-@app.route('/a')
-def render_home():
-    return render_template("home.html")
-
-
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def render_results():
-    #zip_code = request.form['zipCode']
-    data = get_weather_results(90703, API_KEY)
+    init_zip = 90001
+    message = ""
+
+    if request.method == 'POST':
+        zip_code = request.form['zipCode']
+        if check_valid_zip(zip_code):
+            data = get_weather_results(zip_code, API_KEY)
+        else:
+            data = get_weather_results(init_zip, API_KEY)
+            message += "{} is not a valid zip code.".format(zip_code)
+    else:
+        data = get_weather_results(init_zip, API_KEY)
 
     date = datetime.now()
     today = date.strftime("%A %B %d %Y")
 
     temp = "{0:.1f}°F".format(data["main"]["temp"])
-    hilow = "{0:.1f}°F - {0:.1f}°F".format(data["main"]["temp_min"], data["main"]["temp_max"])
+    hilow = "{0:.0f}°F - {0:.0f}°F".format(data["main"]["temp_min"], data["main"]["temp_max"])
     weather = data["weather"][0]["main"]
-    descript = data["weather"][0]["description"]
     location = data["name"]
-    print(temp, hilow, weather, location, descript)
+    print(temp, hilow, weather, location)
 
     return render_template('main.html', location=location, temp=temp,
-                           hilow=hilow, weather=weather, date=today, detail=descript)
+                           hilow=hilow, weather=weather, date=today, message=message)
 
 
-def get_api_key():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    return config['openweathermap']['api']
+def check_valid_zip(zip_code):
+    print("here.....")
+    code = int(zip_code)
+    if 500 < code < 99950:
+        return True
+    return False
 
 
 def get_weather_results(zip_code, api_key):
     api_url = "http://api.openweathermap.org/" \
               "data/2.5/weather?zip={}&units=imperial&appid={}".format(zip_code, api_key)
-    r = requests.get(api_url)
-    return r.json()
+    res = requests.get(api_url)
+    return res.json()
 
 
 if __name__ == '__main__':
