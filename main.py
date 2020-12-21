7cde55790d6169bf19829502866baa8c1c48866b
daivuong <paulminhdai@gmail.com>
2020-12-20 22:38:03 -0800

    fix .env file after pull

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ def call_api(zip_code, api_key):
 
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8080, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=8080, threaded=True, debug=True)
